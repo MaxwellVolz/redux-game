2080821b4efe01fba5d5a8f7bb66346983aea7df
Maxwell Volz <maxwellvolz@gmail.com>
2019-11-05 08:44:12 -0800

    redux container and reducer setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-
-function App() {
-  return (
-    <div className="App">
-      <h1>TODO: Create a game with React, Redux, and SVG!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.message}</h1>
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default App;

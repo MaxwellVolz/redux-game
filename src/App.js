942e41c84c758b04946fea9dfb34fba84dd570c3
Maxwell Volz <maxwellvolz@gmail.com>
2019-11-06 08:25:07 -0800

    dynamic enemy generation

--- src/App.js
+++ src/App.js
@@ -4,45 +4,52 @@ import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
 
 class App extends Component {
-  componentDidMount() {
-    const self = this;
-    setInterval(() => {
-      self.props.moveObjects(self.canvasMousePosition);
-    }, 10);
+	componentDidMount() {
+		const self = this;
+		setInterval(() => {
+			self.props.moveObjects(self.canvasMousePosition);
+		}, 10);
 
-    window.onresize = () => {
-      const cnv = document.getElementById('aliens-go-home-canvas');
-      cnv.style.width = `${window.innerWidth}px`;
-      cnv.style.height = `${window.innerHeight}px`;
-    };
-    window.onresize();
-  }
+		window.onresize = () => {
+			const cnv = document.getElementById('aliens-go-home-canvas');
+			cnv.style.width = `${window.innerWidth}px`;
+			cnv.style.height = `${window.innerHeight}px`;
+		};
+		window.onresize();
+	}
 
-  trackMouse(event) {
-    this.canvasMousePosition = getCanvasPosition(event);
-  }
+	trackMouse(event) {
+		this.canvasMousePosition = getCanvasPosition(event);
+	}
 
-  render() {
-    return (
-      <Canvas
-        angle={this.props.angle}
-        gameState={this.props.gameState}
-        startGame={this.props.startGame}
-        trackMouse={event => (this.trackMouse(event))}
-      />
-    );
-  }
+	render() {
+		return (
+			<Canvas
+				angle={this.props.angle}
+				gameState={this.props.gameState}
+				startGame={this.props.startGame}
+				trackMouse={event => (this.trackMouse(event))}
+			/>
+		);
+	}
 }
 
 App.propTypes = {
-  angle: PropTypes.number.isRequired,
-  gameState: PropTypes.shape({
-    started: PropTypes.bool.isRequired,
-    kills: PropTypes.number.isRequired,
-    lives: PropTypes.number.isRequired,
-  }).isRequired,
-  moveObjects: PropTypes.func.isRequired,
-  startGame: PropTypes.func.isRequired,
+	angle: PropTypes.number.isRequired,
+	gameState: PropTypes.shape({
+		started: PropTypes.bool.isRequired,
+		kills: PropTypes.number.isRequired,
+		lives: PropTypes.number.isRequired,
+		flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+			position: PropTypes.shape({
+				x: PropTypes.number.isRequired,
+				y: PropTypes.number.isRequired
+			}).isRequired,
+			id: PropTypes.number.isRequired,
+		})).isRequired,
+	}).isRequired,
+	moveObjects: PropTypes.func.isRequired,
+	startGame: PropTypes.func.isRequired,
 };
 
 export default App;
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -11,52 +11,59 @@ import StartGame from './StartGame';
 import Title from './Title';
 
 const Canvas = (props) => {
-  const gameHeight = 1200;
-  const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
-  return (
-    <svg
-      id="aliens-go-home-canvas"
-      preserveAspectRatio="xMaxYMax none"
-      onMouseMove={props.trackMouse}
-      viewBox={viewBox}
-    >
-      <defs>
-        <filter id="shadow">
-          <feDropShadow dx="1" dy="1" stdDeviation="2" />
-        </filter>
-      </defs>
-      <Sky />
-      <Ground />
-      <CannonPipe rotation={props.angle} />
-      <CannonBase />
-      <CurrentScore score={15} />
+	const gameHeight = 1200;
+	const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
+	return (
+		<svg
+			id="aliens-go-home-canvas"
+			preserveAspectRatio="xMaxYMax none"
+			onMouseMove={props.trackMouse}
+			viewBox={viewBox}
+		>
+			<defs>
+				<filter id="shadow">
+					<feDropShadow dx="1" dy="1" stdDeviation="2" />
+				</filter>
+			</defs>
+			<Sky />
+			<Ground />
+			<CannonPipe rotation={props.angle} />
+			<CannonBase />
+			<CurrentScore score={15} />
 
-      { ! props.gameState.started &&
-        <g>
-          <StartGame onClick={() => props.startGame()} />
-          <Title />
-        </g>
-      }
+			{!props.gameState.started &&
+				<g>
+					<StartGame onClick={() => props.startGame()} />
+					<Title />
+				</g>
+			}
 
-      { props.gameState.started &&
-        <g>
-          <FlyingObject position={{x: -150, y: -300}}/>
-          <FlyingObject position={{x: 150, y: -300}}/>
-        </g>
-      }
-    </svg>
-  );
+			{props.gameState.started &&
+				<g>
+					<FlyingObject position={{ x: -150, y: -300 }} />
+					<FlyingObject position={{ x: 150, y: -300 }} />
+				</g>
+			}
+
+			{props.gameState.flyingObjects.map(flyingObject => (
+				<FlyingObject
+					key={flyingObject.id}
+					position={flyingObject.position}
+				/>
+			))}
+		</svg>
+	);
 };
 
 Canvas.propTypes = {
-  angle: PropTypes.number.isRequired,
-  gameState: PropTypes.shape({
-    started: PropTypes.bool.isRequired,
-    kills: PropTypes.number.isRequired,
-    lives: PropTypes.number.isRequired,
-  }).isRequired,
-  trackMouse: PropTypes.func.isRequired,
-  startGame: PropTypes.func.isRequired,
+	angle: PropTypes.number.isRequired,
+	gameState: PropTypes.shape({
+		started: PropTypes.bool.isRequired,
+		kills: PropTypes.number.isRequired,
+		lives: PropTypes.number.isRequired,
+	}).isRequired,
+	trackMouse: PropTypes.func.isRequired,
+	startGame: PropTypes.func.isRequired,
 };
 
 export default Canvas;

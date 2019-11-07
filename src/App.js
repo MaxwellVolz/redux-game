9729707f916fab5acde216ca781e6504ef0afa5d
Maxwell Volz <maxwellvolz@gmail.com>
2019-11-06 16:42:09 -0800

    placeholder leaderboard setup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
 
+// Auth0
+import * as Auth0 from 'auth0-web';
+
+Auth0.configure({
+	domain: 'dev-rp1aph5p.auth0.com',
+	clientID: 'hM0weSxRMpM66COZ6VI4ywdy1hm42mbo',
+	redirectUri: 'http://localhost:3000/',
+	responseType: 'token id_token',
+	scope: 'openid profile manage:points',
+});
+
 class App extends Component {
 	componentDidMount() {
 		const self = this;
+
+		Auth0.handleAuthCallback();
+
+		Auth0.subscribe((auth) => {
+			console.log(auth);
+		});
+
 		setInterval(() => {
 			self.props.moveObjects(self.canvasMousePosition);
 		}, 10);
